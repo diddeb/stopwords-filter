9009831f0a16512ab8c33a5e5bb3041fa77ca9a5
Didde Brockman <272143+diddeb@users.noreply.github.com>
2021-01-29 09:43:46 +0100

    Update filter.rb

--- lib/stopwords/snowball/filter.rb
+++ lib/stopwords/snowball/filter.rb
@@ -4,9 +4,13 @@ module Stopwords
       attr_reader :locale
       attr_reader :locale_filename
 
-      def initialize locale, custom_list = []
+      def initialize locale, custom_list = [], custom_file = nil
         @locale = locale
-        @locale_filename = "#{File.dirname(__FILE__)}/locales/#{locale}.csv"
+        @locale_filename = if custom_file.nil?
+          "#{File.dirname(__FILE__)}/locales/#{locale}.csv"
+        else
+          custom_file
+        end
 
         raise ArgumentError.new("Unknown locale: #{locale.inspect}") unless File.exists?(@locale_filename)
         super File.read(@locale_filename).split(",") + custom_list
